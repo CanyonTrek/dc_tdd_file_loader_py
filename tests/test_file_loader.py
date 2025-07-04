1941344c1498ed86dd526f890a1d9a4da2ba55d5
Donald Cameron <donald.cameron@qa.com>
2025-07-04 17:35:12 +0100

    Added Test Mock to test_file_loader and created README_TEST_DOUBLES.md

--- tests/test_file_loader.py
+++ tests/test_file_loader.py
@@ -31,13 +31,35 @@ class TestFileLoader(unittest.TestCase):
         # arrange
         file_to_load = ""
         cut = FileLoader(file_to_load)
-        expected_bytes_read = 10
+        expected_bytes_read = 12
 
         # act
         bytes_read = cut.load_file_with_func(lambda fname: ["Hello", "world"])
 
         # assert
         self.assertEqual(expected_bytes_read, bytes_read)
+
+
+    def test_load_all_of_file_using_mock(self):
+        # Arrange
+        file_to_load = "sample.txt"
+        cut = FileLoader(file_to_load)
+
+        # Simulate file content
+        pretend_file_content = ["Hello", "world"]
+        expected_bytes_read = 10
+
+        # Create a mock callable for reading files
+        mock_file_reader = Mock()
+        mock_file_reader.return_value = pretend_file_content
+
+        # Act
+        # bytes_read = cut.load_file_with_func(lambda fname: mock_file_reader(fname))
+        bytes_read = cut.load_file_with_func(lambda fname: mock_file_reader("XYZ"))
+
+        # Assert
+        self.assertEqual(expected_bytes_read, bytes_read)
+
 
 
 if __name__ == '__main__':
